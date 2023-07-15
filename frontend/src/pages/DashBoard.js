e93ab19b915a8865722ea6a30224d1d316edfa18
shawen17 <shawen022@yahoo.com>
2023-07-15 15:44:39 +0100

    websocket change

--- frontend/src/pages/DashBoard.js
+++ frontend/src/pages/DashBoard.js
@@ -80,7 +80,7 @@ const DashBoard = () => {
     axios.get("https://a2ae-81-24-192-5.ngrok-free.app/api/get_data/", config);
     // Create WebSocket connection
     const newSocket = new WebSocket(
-      "ws://a2ae-81-24-192-5.ngrok-free.app/ws/chart/"
+      "wss://a2ae-81-24-192-5.ngrok-free.app/ws/chart/"
     );
 
     // Store the WebSocket connection in state
